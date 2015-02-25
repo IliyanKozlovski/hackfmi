c60f2d09cfacf75d77bf6b9b7a83a0f8ac95a45e
IliyanKozlovski <i.kozlovski.com@gmail.com>
2015-02-25 21:42:30 +0200

    Fixes

--- optional.rb
+++ optional.rb
@@ -1,16 +1,23 @@
 class Optional
 
+$arr = []
  def initialize(val)
     @val = val
     return val
  end
 
  def method_missing(m, *args)
-    @val = @val.public_send(m, *args)
-    return self
+ 	$arr << m
+	self
  end
  
  def value
+ 	if @val == nil
+ 		puts "nil"
+ 		puts "Missing methods"
+ 		print $arr
+ 	end
+ 	
      return @val
  end
  
@@ -20,8 +27,8 @@ class Optional
  end
 end
 
-#puts Optional.new(nil).no_such_method.value #=> nil
-puts a = Optional.new(1).succ.succ.to_s.value
+#puts Optional.new(nil).no_such_method.fe.value #=> nil
+#p a = Optional.new(14).succ.succ.to_s.value #=> 14
 
 #user = Optional.new(user)
 #p balance = user.account.balance.value #=> nil if user.account is nil
